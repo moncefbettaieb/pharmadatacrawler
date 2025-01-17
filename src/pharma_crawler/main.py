8f6ba48cefbc7e0457f6d13b02d69ef57a35e38f
Moncef Betaieb <moncefbettaieb@gmail.com>
2025-01-17 12:27:07 +0100

    add pharmacie du centre crawler

---
+++ src/pharma_crawler/main.py
@@ -0,0 +1,20 @@
+import sys
+import logging
+import logging.config
+from utils.config import settings
+from modules.scripts.scrappers.save_sitemaps_links_to_mongo import runSitemapLinks
+from modules.scripts.scrappers.save_sitemaps_links_to_mongo import configure_selenium
+from modules.scripts.scrappers.pharma_gdd_scraper import runRunPharmaDataScrapping
+
+def main():
+    logging.config.fileConfig('utils/config/logging.conf')
+    logger = logging.getLogger('scrapper')
+    logger.info("Application Started.")
+    logger.info(f"Environnement : {settings.APP_ENV}")
+    driver = configure_selenium()
+    #runSitemapLinks(driver)
+    runRunPharmaDataScrapping(driver)
+    logger.info("Application terminée.")
+
+if __name__ == "__main__":
+    sys.exit(main())
