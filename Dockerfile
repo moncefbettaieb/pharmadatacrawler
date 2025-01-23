b27ec8bef03d70af7a53cf12eea40bd31033eb5d
Moncef Betaieb <moncefbettaieb@gmail.com>
2025-01-23 17:12:57 +0100

    improve insert scrapped data

--- Dockerfile
+++ Dockerfile
@@ -2,11 +2,10 @@ FROM python:3.11-slim
 
 WORKDIR /app
 
-# Installer les dépendances système
-RUN apt-get update && apt-get install -y \
-    wget \
-    gnupg \
-    unzip \
+# Installer Chromium + Driver + dépendances
+RUN apt-get update && apt-get install -y --no-install-recommends \
+    chromium \
+    chromium-driver \
     libnss3 \
     libxss1 \
     libappindicator3-1 \
@@ -20,23 +19,20 @@ RUN apt-get update && apt-get install -y \
     libgbm1 \
     libu2f-udev \
     libxtst6 \
-    && apt-get clean
+    ca-certificates \
+    wget \
+    gnupg \
+    unzip \
+    && rm -rf /var/lib/apt/lists/*
 
-# Installer Chrome
-RUN apt -f install -y
-RUN apt-get install -y wget
-RUN wget -q https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
-RUN apt-get install ./google-chrome-stable_current_amd64.deb -y
-
-# Copier les fichiers nécessaires
+# Copier votre code
 COPY . .
 
-# Installer les dépendances Python
+# Installer les dépendances Python (Selenium, etc.)
 RUN pip install --no-cache-dir -r requirements.txt
 
-# Ajouter le dossier racine au PYTHONPATH
+# Utiliser le binaire Chromium et le chromedriver installés
 ENV PYTHONPATH="/app"
 
-# Commande par défaut
 ENTRYPOINT ["python", "-m"]
 CMD ["scrappers.save_sitemaps_links_to_mongo"]
--- modules/scrappers/pharma_scrapper.py
+++ modules/scrappers/pharma_scrapper.py
@@ -13,7 +13,10 @@ def process_sitemap_entries(driver, db, last_execution=None):
     Traiter les entrées de la collection `sitemaps` en fonction de last_execution.
     """
     sitemaps_collection = db["sitemaps"]
-
+    if driver is None:
+        driver = settings.configure_selenium()
+    if db is None:
+        db = MongoConnection.get_instance()
     for sitemap_entry in sitemaps_collection.find():
         loc = sitemap_entry.get("loc")
         lastmod = sitemap_entry.get("lastmod")
@@ -37,7 +40,8 @@ def process_sitemap_entries(driver, db, last_execution=None):
                 case "pharmacie-du-centre":
                     scraped_data = pharmacie_du_centre_scrapper.scrape_pharma_du_centre(driver, loc)
             for item in scraped_data:
-                item["source"] = source
+                if item is not None: 
+                    item["source"] = source
                 insert_scraped_data(source, scraped_data, db, last_execution)
 
             print(f"Inserted {len(scraped_data)} items from {loc} into {source}")
@@ -51,24 +55,26 @@ def insert_scraped_data(source, data, db, last_execution=None):
     """
     collection = db[source]
     for item in data:
-        item["processed_time"] = datetime.now()
+        if item is not None:
+            item["processed_time"] = datetime.now()
         collection.insert_one(item)
 
 if __name__ == "__main__":
     logging.config.fileConfig('utils/config/logging.conf')
     logger = logging.getLogger('Pharma Data')
+    logger.info(f"Application Started Environnement : {settings.APP_ENV}.")
     db = MongoConnection.get_instance()
-    logger.info("Application Started.")
-    logger.info(f"Environnement : {settings.APP_ENV}")
     driver = settings.configure_selenium()
     last_execution_arg = sys.argv[1] if len(sys.argv) > 1 else None
     last_execution = (
         datetime.strptime(last_execution_arg, "%Y-%m-%d") if last_execution_arg else None
-    )    
+    )
+    logger.info(f"Last execution was at : {last_execution}.")
     try:
         process_sitemap_entries(driver, db, last_execution)
     except Exception as e:
         logger.error(f"Erreur lors du traitement des entrées de sitemap : {e}")
     finally:
+        logger.error(f"quit driver")
         driver.quit()
     logger.info("Application terminée.")
