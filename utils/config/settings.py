1683ab448a4b6434b4250c68abda6792b1ff6942
Moncef Betaieb <moncefbettaieb@gmail.com>
2025-03-13 11:03:48 +0100

    improve insert scrapped data

--- utils/config/settings.py
+++ utils/config/settings.py
@@ -26,7 +26,7 @@ POSTGRES_CONFIG = {
 
 # Configuration MongoDB
 MONGO_CONFIG = {
-    "host": os.getenv("MONGO_HOST", "host.docker.internal"),
+    "host": os.getenv("MONGO_HOST", "localhost"),
     "port": int(os.getenv("MONGO_PORT", 27017)),
     "username": os.getenv("MONGO_USER", ""),
     "password": os.getenv("MONGO_PASSWORD", ""),
